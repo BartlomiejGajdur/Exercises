7eed65e0acf5553eac7deb7106f0fadcc72d2b99
Bartlomiej <gajdurbartlomiej@gmail.com>
2022-05-30 12:43:19 +0200

    Pointers added

---
+++ Cars/Engine.hpp
@@ -0,0 +1,15 @@
+// #include "PetrolEngine.hpp"
+// #include "ElectricEngine.hpp"
+
+// class Engine
+// {
+// private:
+    
+// public:
+//     PetrolEngine* Petrolengine_;
+//     ElectricEngine* Electricengine_;
+//     Engine(PetrolEngine* petrolEng) : PetrolEngine_(petrolEng) {}
+//     Engine(ElectricEngine* electricEng);
+
+    
+// };
