ae47b6893f26f318b4bbb4679534d4210a671a0b
Bartlomiej <gajdurbartlomiej@gmail.com>
2022-06-05 15:40:24 +0200

    Added changeEngine, working ?

--- Cars/main.cpp
+++ Cars/main.cpp
@@ -6,33 +6,29 @@
 
 int main()
 {
-
-
-
-    
-
-
     std::cout << std::endl << "OPEL" << std::endl;
+
     PetrolCar opel(new PetrolEngine(120, 1800, 6));
-    Car* car = &opel;
+    Car* car = &opel;    
+    PetrolCar* petCar = dynamic_cast<PetrolCar*>(car);
 
-    PetrolEngine petrol(1,2,3);
-
-    //Engine* engine = &petrol;
     car->accelerate(50);
     car->brake();
     car->accelerate(-900);
     car->refill();
-   // car->changeEngine(PetrolEngine(150, 700, 7));
+    petCar->changeEngine(new PetrolEngine(150, 700, 7));
     car->refill();
     
     std::cout << std::endl << "NISSAN" << std::endl;
+
     ElectricCar nissan(new ElectricEngine(130, 650));
     car = & nissan;
+    ElectricCar* eleCar = dynamic_cast<ElectricCar*>(car);
+
     car->refill();
     car->accelerate(80);
     //nissan.engine_ = new ElectricEngine(150, 700);  // Changing an engine during driving is not safe
-    //car->changeEngine(new ElectricEngine(150, 700));
+    eleCar->changeEngine(new ElectricEngine(150, 700));
     car->turnLeft();
 
     std::cout << std::endl << "TOYOTA" << std::endl;
