baa9552821b0d25aced163a65d68ec04707730de
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-08-15 12:27:53 +0200

    Shared_ptr - test

--- Memory_and_exception_managment/test.cpp
+++ Memory_and_exception_managment/test.cpp
@@ -1,20 +1,55 @@
+// shared_ptr-examples.cpp
+// The following examples assume these declarations:
+#include <algorithm>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
 
-int funkcja(){
-    std::cout<<"Hejj"<<std::endl;
-    return 10;
-}
-
-void funkcja2(int a)
+struct MediaAsset
 {
-    std::cout<<"Wynik dodawania 10+ 23 to:"<<std::endl;
-    std::cout<<a+23;
+    virtual ~MediaAsset() = default; // make it polymorphic
+};
+
+struct Song : public MediaAsset
+{
+    std::wstring artist;
+    std::wstring title;
+    Song(const std::wstring& artist_, const std::wstring& title_) :
+        artist{ artist_ }, title{ title_ } {}
+};
+
+struct Photo : public MediaAsset
+{
+    std::wstring date;
+    std::wstring location;
+    std::wstring subject;
+    Photo(
+        const std::wstring& date_,
+        const std::wstring& location_,
+        const std::wstring& subject_) :
+        date{ date_ }, location{ location_ }, subject{ subject_ } {}
+};
+
+using namespace std;
+
+int main()
+{
+   vector<shared_ptr<Song>> v {
+  make_shared<Song>(L"Bob Dylan", L"The Times They Are A Changing"),
+  make_shared<Song>(L"Aretha Franklin", L"Bridge Over Troubled Water"),
+  make_shared<Song>(L"Thalía", L"Entre El Mar y Una Estrella")
+};
+
+vector<shared_ptr<Song>> v2;
+
+remove_copy_if(v.begin(), v.end(), back_inserter(v2), [] (shared_ptr<Song> s) 
+{
+    return s->artist.compare(L"Bob Dylan") == 0;
+});
+
+for (const auto& s : v2)
+{
+    wcout << s->artist << L":" << s->title << endl;
 }
-
-int main(){
-
-    funkcja2(funkcja());
-
-
-    return 0;
 }
