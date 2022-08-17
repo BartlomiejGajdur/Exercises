17bd3fede5c4ac5b356000ef479c8213b2b8934a
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-08-17 11:46:02 +0200

    Implementation of template class unique_ptr with Operator* and some methods

--- Memory_and_exception_managment/unique_ptr.cpp
+++ Memory_and_exception_managment/unique_ptr.cpp
@@ -1,8 +1,39 @@
 /* Tworzenie własnego unique_ptr*/
-
+#include <iostream>
 namespace cs{
-
+template <typename T>
 class unique_ptr {
+private:
+    T* ptr_{nullptr}; //inicjalizacja nullptr aby zapobiec UB
+
+public:
+    unique_ptr(T* ptr = nullptr) : ptr_{ptr}{}
+    ~unique_ptr(){
+        delete ptr_;
+    };
+
+    T& operator*() const{
+        return *ptr_;
+    }
+
+    T* get() const{
+        return ptr_;
+    }
+
+    T* release(){
+        T* temp_ptr = ptr_;
+        ptr_ = nullptr;
+        return temp_ptr;
+    }
+
+    void reset(T* ptr = nullptr){ //dodac nullptr
+        delete ptr_;
+        ptr_ = ptr;
+    }
+
+    void reset(){   // sprawdzic te funkcje 
+        delete ptr_;
+    }
 
 
 };
@@ -16,13 +47,13 @@ int main(){
 cs::unique_ptr ptr{new int{5}};
 std::cout<< *ptr<<'\n';
 std::cout<< ptr.get()<<'\n';
-std::cout<< ptr.release()<<'\n';
+auto rawPtr =  ptr.release();
+delete rawPtr;
+// //cs::unique_ptr<int> ptr2 = ptr; //Zabronione
+// cs::unique_ptr<int> ptr2 = std::move(ptr);
 
-//cs::unique_ptr<int> ptr2 = ptr; //Zabronione
-cs::unique_ptr<int> ptr2 = std::move(ptr);
-
-ptr.reset(new int {10});
-ptr.reset();
+// ptr.reset(new int {10});
+// ptr.reset();
 
 
 
