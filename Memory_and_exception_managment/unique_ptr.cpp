0f1906d3eba10ca5d708ddaa51795329414e420d
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-08-17 14:54:24 +0200

    Full implementation of unique_ptr

--- Memory_and_exception_managment/unique_ptr.cpp
+++ Memory_and_exception_managment/unique_ptr.cpp
@@ -12,8 +12,18 @@ public:
     unique_ptr(T* ptr = nullptr) : ptr_{ptr}{}
 
     unique_ptr(const unique_ptr<T>&) =delete; //Konstruktor kopiujący jako delete (zakazany)                         (1)
-    unique_ptr<T> operator=(const unique_ptr<T>&) = delete; // Operator przypisania kopiujący jako delete (zakazany) (2)
+    unique_ptr<T>& operator=(const unique_ptr<T>&) = delete; // Operator przypisania kopiujący jako delete (zakazany) (2)
 
+    unique_ptr(unique_ptr<T>&& otherPtr) : ptr_{otherPtr.release()} {}
+    unique_ptr<T>& operator=(unique_ptr<T>&& otherPtr){
+        if(this != &otherPtr)  // this i otherPtr nie wskazuja na ten sam adres
+        {
+            delete ptr_;
+            ptr_= otherPtr.get();
+            otherPtr.ptr_ = nullptr;
+        }
+        return *this;
+    }
     ~unique_ptr(){
         delete ptr_;
     };
@@ -56,14 +66,13 @@ std::cout<< ptr.get()<<'\n';
 auto rawPtr =  ptr.release();
 delete rawPtr;
 
-
-cs::unique_ptr<int> ptr2 = ptr;          // (1)
-cs::unique_ptr<int> ptr3{new int {5}};
-ptr3 = ptr;                              // (2)
-
+//cs::unique_ptr<int> ptr2 = ptr;          // (1)
+//cs::unique_ptr<int> ptr3{new int {5}};
+//ptr3 = ptr;                              // (2)
 
 
-
+cs::unique_ptr<int> ptr2{new int{2}};
+ptr2 = std::move(ptr);
 
  ptr.reset(new int {10});
  std::cout<< *ptr<<std::endl;
