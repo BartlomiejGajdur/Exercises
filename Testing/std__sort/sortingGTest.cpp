e0efbc5d11390e3b265dda01eccf7cd5092dac3e
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-17 12:37:11 +0200

    Changed everything to parametric tets

--- Testing/std__sort/sortingGTest.cpp
+++ Testing/std__sort/sortingGTest.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm> 
+#include <functional>
 
 struct SortTestUnderTestFixture : public ::testing::Test{
     std::vector<int> v1{1,2,3,4,5}; //sorted
@@ -12,136 +13,89 @@ struct SortTestUnderTestFixture : public ::testing::Test{
     std::string text{"text"};
 };
 
-struct SortTestFixture : public ::testing::TestWithParam<std::vector<int>>{
-    std::vector<int> v{ 3,2,1};
-    std::vector<int> expected {1,2,3};
 
-    template<typename Type>
-    void sortAndCheck(Type & input,
-                      const Type & expected){
-         //When  
-        std::sort(input.begin(),input.end());
+using StringPair = std::pair<std::string,std::string>;
 
-        //Then
-        ASSERT_EQ(input,expected);
-     }
+struct SortStringTextFixture : public ::testing::TestWithParam<StringPair>
+{};
 
-    template<typename Type>
-    void sortDescendingAndCheck(Type & input,
-                      const Type & expected){
-         //When  
-        std::sort(input.begin(),input.end(), std::greater<int>{});
+TEST_P(SortStringTextFixture, SortString_text__xtte)
+{
+    //Given
+    auto [input, expected] = GetParam();
+    
+    //When
+    std::sort(input.begin(),input.end());
 
-        //Then
-        ASSERT_EQ(input,expected);
-     }
+    //THEN
+    ASSERT_EQ(input,expected);
+
+}
+
+INSTANTIATE_TEST_SUITE_P(PermutationsOfString,
+                        SortStringTextFixture,
+                        testing::Values(StringPair{{"text"},{"ettx"}},
+                                        StringPair{{"ettx"},{"ettx"}},
+                                        StringPair{{"tExTtextTEXT"},{"EETTTXetttxx"}}
+                                        ));
+
+
+using Comparator = std::function<bool(const int&a, const int&b)>;
+using VectorsPair = std::tuple<std::vector<int>,std::vector<int>, Comparator>;
+
+struct SortTestFixture : public ::testing::TestWithParam<VectorsPair>{
+    // std::vector<int> v{ 3,2,1};
+    // std::vector<int> expected {1,2,3};
+
+    // template<typename Type>
+    // void sortAndCheck(Type & input,
+    //                   const Type & expected){
+    //      //When  
+    //     std::sort(input.begin(),input.end());
+
+    //     //Then
+    //     ASSERT_EQ(input,expected);
+    //  }
+
+    // template<typename Type>
+    // void sortDescendingAndCheck(Type & input,
+    //                   const Type & expected){
+    //      //When  
+    //     std::sort(input.begin(),input.end(), std::greater<int>{});
+
+    //     //Then
+    //     ASSERT_EQ(input,expected);
+    //  }
 
 };
 
-
-
-TEST_F(SortTestUnderTestFixture, SortString_text_ettx)
-{
-    //Given
-    std::string expected{"ettx"};
-
-    //When  
-    std::sort(text.begin(),text.end());
-
-    //Then
-    ASSERT_EQ(text,expected);
-}
-
-
-TEST_F(SortTestUnderTestFixture, SortString_tExTtextTEXT_EETTTXetttxx)
-{
-    //Given
-    std::string textw{"tExTtextTEXT"};
-    std::string expected{"EETTTXetttxx"};
-
-    //When  
-    std::sort(textw.begin(),textw.end());
-
-    //Then
-    ASSERT_EQ(textw,expected);
-}
-
-TEST_F(SortTestUnderTestFixture, SortVector)
-{
-    //Given
-    std::vector<int> expected{1,2,3,4,5};
-
-    //When  
-    std::sort(v2.begin(),v2.end());
-
-    //Then
-    ASSERT_EQ(v2,expected);
-}
-
-TEST_F(SortTestUnderTestFixture, VectorVisSortedWithReverseIterators)
-{
-    //Given
-    std::vector<int> expected {5, 4, 3, 2, 1};
-
-    //When  
-    std::sort(v1.rbegin(),v1.rend());
-
-    //Then
-    ASSERT_EQ(v1,expected);
-}
-
-TEST_F(SortTestUnderTestFixture, EmptyVectors)
-{
-    //Given
-    std::vector<int> expected {};
-
-    //When  
-    std::sort(v5.begin(),v5.end());
-
-    //Then
-    ASSERT_EQ(v5,expected);
-}
-
-//@@@@//
-
 TEST_P(SortTestFixture, GivenPermutedVector){
     
-    std::vector<int> v = GetParam();
-    std::vector<int> expected {1,2,3};
+    
+    auto [input, expected, comparator] = GetParam(); //C++17 is equal to:
+    /*
+    auto pair = GetParam();
+    auto v = pair.first;
+    auto expected = pair.second;
+    first and second are member objects.
+    */
 
-    sortAndCheck(v,expected);
+    //When  
+     std::sort(input.begin(),input.end(),comparator);
+
+    //Then
+     ASSERT_EQ(input,expected);
 }
 
 INSTANTIATE_TEST_SUITE_P(PermutationsOf123Vector,
                         SortTestFixture,
-                        testing::Values(std::vector<int>{3,2,1},
-                                        std::vector<int>{1,2,3},
-                                        std::vector<int>{2,1,3},
-                                        std::vector<int>{3,1,2}));
-
-TEST_F(SortTestFixture ,VShouldBeEqualTo321From123)
-{
-    std::vector<int> v {1,2,3};
-    std::vector<int> expected {3,2,1};
-
-    sortDescendingAndCheck(v,expected);
-}
-
-TEST_F(SortTestFixture, SortString_text__xtte)
-{
-    //Given
-    std::string text{"text"};
-    std::string expected{"xtte"};
-
-    sortDescendingAndCheck(text,expected);
-}
-
-TEST_F(SortTestFixture, SortString_tExTtextTEXT__xxttteXTTTEE)
-{
-    //Given
-    std::string textw{"tExTtextTEXT"};
-    std::string expected{"xxttteXTTTEE"};
-
-    sortDescendingAndCheck(textw,expected);
-}
-
+                        testing::Values(VectorsPair{{3,2,1},{1,2,3}, std::less<int>{}},
+                                        VectorsPair{{1,2,3},{1,2,3}, std::less<int>{}},
+                                        VectorsPair{{2,1,3},{1,2,3}, std::less<int>{}},
+                                        VectorsPair{{3,1,2},{1,2,3}, std::less<int>{}},
+                                        VectorsPair{{},{}, std::less<int>{}},
+                                        VectorsPair{{1},{1}, std::less<int>{}},
+                                        VectorsPair{{5,4,3,2,1},{1,2,3,4,5}, std::less<int>{}},
+                                        VectorsPair{{1,2,3},{3,2,1}, std::greater<int>{}},
+                                        VectorsPair{{1,2,3,4,5},{5,4,3,2,1}, std::greater<int>{}}
+                                        ));
