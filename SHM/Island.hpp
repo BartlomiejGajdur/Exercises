aac22185b841cad0778108465ccabf092ef875fe
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-19 18:21:31 +0200

    C-tor and Getter added

---
+++ SHM/Island.hpp
@@ -0,0 +1,21 @@
+#include "Coordinates.hpp"
+
+class Island{
+    public:
+        Island(Coordinates position) : position_(position) {}
+    
+
+    //GETTERS
+    Coordinates getPosition() {return position_;}
+
+    //SETTERS
+
+
+    //OPERATORS
+
+
+    private:
+        Coordinates position_;
+
+
+};
