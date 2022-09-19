726b5a23b7ed20bdc3e5825720b236e5fc9f76c7
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-19 20:14:28 +0200

    Default C-tor with random generate Position for Islands added

--- SHM/Island.hpp
+++ SHM/Island.hpp
@@ -1,10 +1,17 @@
 #pragma once
 #include "Coordinates.hpp"
+#include <random>
 
 class Island{
     public:
         Island(Coordinates position) : position_(position) {}
-    
+        Island(){
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> distrib(1, 100);
+            position_.setPositionX(distrib(gen));
+            position_.setPositionY(distrib(gen));
+        }
 
     //GETTERS
     Coordinates getPosition() {return position_;}
@@ -13,10 +20,17 @@ class Island{
 
 
     //OPERATORS
-
+    friend std::ostream& operator<<(std::ostream &os, Island& island);
 
     private:
         Coordinates position_;
 
 
 };
+
+
+std::ostream& operator<<(std::ostream &os, Island& island)
+    {       
+        os<<island.getPosition();
+        return os;
+    }
