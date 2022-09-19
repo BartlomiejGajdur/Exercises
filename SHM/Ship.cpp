b05762b156b86460e1caf4ba15ffa76122475539
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-19 20:10:39 +0200

    Some tests

--- SHM/Ship.cpp
+++ SHM/Ship.cpp
@@ -2,6 +2,7 @@
 #include "Cargo.hpp"
 #include "Coordinates.hpp"
 #include "Island.hpp"
+#include "Map.hpp"
 #include <string>
 #include <iostream>
 #include <ostream>
@@ -9,6 +10,24 @@
 int main(){
 
     Coordinates pierwsze{1,5};
-    std::cout<<pierwsze;
+    std::cout<<pierwsze<<std::endl;
+
+    Island wyspa[5]{};
+    std::cout<<wyspa[0].getPosition()<<std::endl;
+    std::cout<<wyspa[1].getPosition()<<std::endl;
+    std::cout<<wyspa[2].getPosition()<<std::endl;
+    std::cout<<wyspa[3].getPosition()<<std::endl;
+    std::cout<<wyspa[4].getPosition()<<std::endl;
+
+    std::cout<<std::endl<<std::endl;
+
+    Map mapa;
+    mapa.getAllIslands();
+    for(auto v :mapa.getAllIslands() )
+    {
+        std::cout<<v<<std::endl;
+    }
+
+
     return 0;
 }
