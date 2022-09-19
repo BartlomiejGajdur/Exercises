d554126d7afc9897ffb837cfab15accc4c4c0365
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-19 20:10:19 +0200

    C-tor added

--- SHM/Map.hpp
+++ SHM/Map.hpp
@@ -2,13 +2,22 @@
 #include <memory>
 #include <vector>
 #include "Island.hpp"
+#include <random>
 
 class Map{
     public:
-
+     Map(){
+        Island islands[10]{};
+        for (int i =0 ; i<10; i++)
+        {
+            allIslands_.push_back(islands[i]);
+        }
+     }
+    
+    std::vector<Island> getAllIslands() const {return allIslands_;}
 
     private:
-    std::vector<std::shared_ptr<Island>> allIslands_;
+    std::vector<Island> allIslands_;
     std::shared_ptr<Island> currentPosition_; //Should have been unique_ptr ?
 
 };
