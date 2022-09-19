0e2a938bd5fc3632b8e80a8390ec588807647663
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-19 14:40:55 +0200

    Added some operators

--- SHM/Ship.hpp
+++ SHM/Ship.hpp
@@ -7,21 +7,22 @@ class Ship{
          std::string name, 
          size_t speed, 
          size_t maxCrew,
-         size_t capacity): id_(id), name_(name), speed_(speed), maxCrew_(maxCrew), capacity_(capacity) {}
+         size_t capacity): id_(id), name_(name), speed_(speed), maxCrew_(maxCrew), capacity_(capacity), crew_(0) {}
 
-    Ship() : id_(-1) {}
+    Ship() : id_(-1), crew_(0) {}
 
     Ship(int id, 
          size_t speed, 
          size_t maxCrew): Ship(id,"",speed,maxCrew,0) {}
 
     //Getters
-    int getId() const { return id_;}
-    std::string getName() const { return name_;}
-    size_t getSpeed() const { return speed_;}
-    size_t getMaxCrew() const { return maxCrew_;}
-    size_t getCapacity() const { return capacity_;}
-
+    int getId()             const { return id_;}
+    std::string getName()   const { return name_;}
+    size_t getSpeed()       const { return speed_;}
+    size_t getMaxCrew()     const { return maxCrew_;}
+    size_t getCapacity()    const { return capacity_;}
+    size_t getCrew()        const { return crew_;}
+    
     //Setters
     void setName(const std::string &name) {name_ = name;}
 
