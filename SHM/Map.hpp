4d5cca0748ae7cd0c42a4eef9f9dbea85de2cfdb
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-20 13:27:38 +0200

    ValidateGenerated Islands

--- SHM/Map.hpp
+++ SHM/Map.hpp
@@ -7,14 +7,42 @@
 class Map{
     public:
      Map(){
-        std::shared_ptr<Island> islands[10]{};
-        for (int i =0 ; i<10; i++)
-        {
-            allIslands_.push_back(islands[i]);
-        }
+        do{
+            generateIslandsOnMap(allIslands_);
+        }while(validateGeneratedIslands(allIslands_));
+
+        //wykonuj dopóki nie beda wszystkie inne
+            
      }
-    
+
+
+    //GETTERS
    std::vector<std::shared_ptr<Island>> getAllIslands() const {return allIslands_;}
+
+   //FUNCTIONS
+   void generateIslandsOnMap ( std::vector<std::shared_ptr<Island>> &islands) // Map contains vector of shared_ptr<Island> with random generate Islands
+   {
+        for (int i = 0; i<10 ; i++) 
+        {
+            islands.push_back(std::make_shared<Island>());
+        }
+   }
+
+   bool validateGeneratedIslands(std::vector<std::shared_ptr<Island>> &allIslands_) //Check if every island is unique
+    {
+
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < i; j++)
+                {
+                    if(*allIslands_[j] == *allIslands_[i])
+                    {
+                        return true;
+                    }
+                }
+        }
+        return false;
+    }
 
     private:
     std::vector<std::shared_ptr<Island>> allIslands_;
