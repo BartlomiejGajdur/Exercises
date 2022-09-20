99dea44101b329a173a233548684417ff868f0be
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-20 11:07:51 +0200

    std::shared_ptr added

--- SHM/Map.hpp
+++ SHM/Map.hpp
@@ -7,17 +7,17 @@
 class Map{
     public:
      Map(){
-        Island islands[10]{};
+        std::shared_ptr<Island> islands[10]{};
         for (int i =0 ; i<10; i++)
         {
             allIslands_.push_back(islands[i]);
         }
      }
     
-    std::vector<Island> getAllIslands() const {return allIslands_;}
+    std::vector<std::shared_ptr<Island>> getAllIslands() const {return allIslands_;}
 
     private:
-    std::vector<Island> allIslands_;
+    std::vector<std::shared_ptr<Island>> allIslands_;
     std::shared_ptr<Island> currentPosition_; //Should have been unique_ptr ?
 
 };
