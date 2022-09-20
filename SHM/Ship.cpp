20d4a4d7e9e1443b84d866ea3886379fbafa4bb6
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-20 13:27:00 +0200

    Refactor

--- SHM/Ship.cpp
+++ SHM/Ship.cpp
@@ -9,24 +9,44 @@
 
 int main(){
 
-    Coordinates pierwsze{1,5};
-    std::cout<<pierwsze<<std::endl;
+    // Coordinates pierwsze{1,5};
+    // std::cout<<pierwsze<<std::endl;
 
-    Island wyspa[5]{};
-    std::cout<<wyspa[0].getPosition()<<std::endl;
-    std::cout<<wyspa[1].getPosition()<<std::endl;
-    std::cout<<wyspa[2].getPosition()<<std::endl;
-    std::cout<<wyspa[3].getPosition()<<std::endl;
-    std::cout<<wyspa[4].getPosition()<<std::endl;
+    // //Island wyspa[5]{};
+    // std::cout<<wyspa[0].getPosition()<<std::endl;
+    // std::cout<<wyspa[1].getPosition()<<std::endl;
+    // std::cout<<wyspa[2].getPosition()<<std::endl;
+    // std::cout<<wyspa[3].getPosition()<<std::endl;
+    // std::cout<<wyspa[4].getPosition()<<std::endl;
 
-    std::cout<<std::endl<<std::endl;
+    // std::cout<<std::endl<<std::endl;
 
     Map mapa;
     mapa.getAllIslands();
     for(auto v :mapa.getAllIslands() )
     {
-        std::cout<<v<<std::endl;
-    }
+        std::cout<<v->getPosition()<<std::endl;
+    }   
+
+    // std::vector<std::shared_ptr<Island>> wszystkie;
+    // wszystkie = mapa.getAllIslands();
+    // *wszystkie[0] = Island({1,2});
+    // *wszystkie[1] = Island({1,2});
+    
+    // std::cout<<"\n\n";
+    // for(auto v :wszystkie)
+    // {
+    //     std::cout<<v->getPosition()<<std::endl;
+    // }   
+
+    // if(*wszystkie[0]==*wszystkie[1])
+    // {
+    //     std::cout<<"tak";
+    // }else{
+    //     std::cout<<"nie";
+    // }
+
+
     
 
 
