a1c6c3451c0bcdc5f2ff57d0f76fe50c0bb4184d
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-21 14:17:46 +0200

    C-tor added

--- SHM/Player.hpp
+++ SHM/Player.hpp
@@ -1,12 +1,16 @@
 #pragma once
 #include <memory>
+#include <iostream>
 #include "Ship.hpp"
 
 
 class Player{
     public:
-
-
+    Player(std::shared_ptr<Ship> ship, double money, int availableSpace): 
+                                                                    ship_(ship), 
+                                                                    money_(money),
+                                                                    availableSpace_(availableSpace) {}
+    
 
     private:
     std::shared_ptr<Ship> ship_;
