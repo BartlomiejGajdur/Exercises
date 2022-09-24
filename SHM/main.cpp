59d96b9e210a374ddee0cbde7f8c0d33be5ad257
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-09-24 12:28:41 +0200

    Main modify

--- SHM/main.cpp
+++ SHM/main.cpp
@@ -12,6 +12,9 @@
 #include "Island.cpp"
 #include "Player.hpp"
 #include "Player.cpp"
+#include "Alcohol.hpp"
+#include <cmath>
+#include <iomanip>
 
 
 int main(){
@@ -39,6 +42,30 @@ int main(){
     --owoc;
     std::cout<<"GetPrice: "<<owoc.getPrice()<<std::endl;
     std::cout<<owoc.getTimeToSpoil()<<std::endl;
-    
+    std::cout<<"\n\n\n";
+
+    double baseprice = 40;
+    size_t percentage = 96; // base price dla 96 percentage to jest 40. /40
+    std::cout << "exp(1) = e¹ = "  <<std::setprecision(2)<< static_cast<double>(std::exp(percentage/baseprice)) << '\n';
+    percentage=86;
+    std::cout << "exp(1) = e¹ = "  <<std::setprecision(2)<< static_cast<double>( std::exp(percentage/baseprice) )<< '\n';
+     percentage=76;
+    std::cout << "exp(1) = e¹ = " << std::exp(percentage/baseprice) << '\n';
+     percentage=66;
+    std::cout << "exp(1) = e¹ = "  << std::exp(percentage/baseprice) << '\n';
+    percentage=56;
+    std::cout << "exp(1) = e¹ = "  << std::exp(percentage/baseprice) << '\n';
+
+    std::cout<<"\n\n\n";
+    Alcohol spirol{"SpirytusSalicynowy",10,40,96};
+    std::cout<<spirol.getName()<<": "<<spirol.getPrice()<<"\n";
+    Alcohol Piwo{"Harnas",10,40,10};
+    std::cout<<Piwo.getName()<<": "<<Piwo.getPrice()<<"\n";
+    Alcohol spirol1{"Spiryynowy",10,40,40};
+    std::cout<<spirol1.getName()<<": "<<spirol1.getPrice()<<"\n";
+    Alcohol spirol2{"Spiryy123nowy",10,40,70};
+    std::cout<<spirol2.getName()<<": "<<spirol2.getPrice()<<"\n";
+
+
     return 0;
 }
