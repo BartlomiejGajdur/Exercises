b08ddbdb2f595bb277780c1402f5f0a480e591ac
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-10-05 20:06:26 +0200

    Observer - Design Pattern

---
+++ DesignPaterns/Observer/Observer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+class IObserver {
+ public:
+  virtual ~IObserver(){};
+  virtual void Update(const std::string &message_from_subject) = 0;
+};
+
+class ISubject {
+ public:
+  virtual ~ISubject(){};
+  virtual void Attach(IObserver *observer) = 0;
+  virtual void Detach(IObserver *observer) = 0;
+  virtual void Notify() = 0;
+};
+
+int main(){
+
+    std::cout<<"Hello WOrld";
+
+    return 0;
+}
