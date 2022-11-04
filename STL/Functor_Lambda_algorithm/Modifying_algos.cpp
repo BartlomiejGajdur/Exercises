98953a5cacc2afb5632cbfd9021ddb2e9c140cb4
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-11-04 12:16:39 +0100

    Exercises

--- STL/Functor_Lambda_algorithm/Modifying_algos.cpp
+++ STL/Functor_Lambda_algorithm/Modifying_algos.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <iterator>
 #include <random>
+#include<list>
+#include <deque>
+#include <map>
 /*
 1. Utwórz poniższy wektor:
 
@@ -24,12 +27,57 @@ std::vector<std::pair<int, std::string>> v {
 8. Pomieszaj losowo wszystkie elementy
 
 9. Wypisz je raz jeszcze
+
+---------------------------------------------------------
+
+10.[insensitive-palindrom] Napisz program, który będzie sprawdzał, czy podany ciąg wyrazów jest palindromem. 
+Program powinien ignorować znaki specjalne np. ,.?()[] (i nie tylko). 
+Powinien również ignorować wielkość liter oraz białe znaki jak spacja czy znak nowej linii.
+
+11. [transform-containers] Napisz funkcję, która:
+ a) Przyjmie std::list<std::string> oraz std::deque<int>
+ b) Usunie duplikaty z obu kontenerów
+ c) Na koniec skonwertuje to na std::map<int, std::string> i ją zwróci. Użyj std::transform.
 */
 template<typename T>
     void printVector(const std::vector<T>& v){
         std::for_each(v.begin(),v.end(),[](const auto a){std::cout<<a<<" ";});
         std::cout<<"\n";
     }
+
+bool insensitivePalindrom(const std::string& text){
+    std::string b = text;
+
+    std::transform(b.begin(),b.end(),b.begin(),[](char& a){return tolower(a);});
+    b.erase(std::remove_if(b.begin(),b.end(),[](const char a){return a<97 || a>122;}),b.end());
+
+    return std::equal(b.begin(), b.begin() + b.size()/2,b.rbegin());
+}
+
+void testInsensitivePalindrom(const std::string& text)
+{
+    std::cout<<"Word: "<<"\""<<text<<"\""<<(insensitivePalindrom(text)?" is" : " is not")<<" a insensitive Palindrom :)\n";
+}
+
+std::map<int, std::string> converter(std::list<std::string> lista, std::deque<int> kolejka){
+
+    std::map<int, std::string> mapa;
+
+    lista.sort();
+    lista.erase(std::unique(lista.begin(),lista.end()),lista.end());
+
+    std::sort(kolejka.begin(),kolejka.end());
+    kolejka.erase(std::unique(kolejka.begin(),kolejka.end()),kolejka.end());
+
+    std::transform(lista.begin(),lista.end(),kolejka.begin(),std::inserter(mapa,mapa.end()),
+    [&](auto a, auto b)
+    {   
+      return std::pair<int,std::string>(b,a);
+      
+    });
+
+    return mapa;
+}
 
 int main(){
     // #1
@@ -76,5 +124,26 @@ int main(){
     std::mt19937 g(rd());
     std::shuffle(v5.begin(), v5.end(), g);
     printVector(v5);
+    std::cout<<"\n------------------------------------------------------------\n\n";
+    
+    // #10
+    std::string a = "KAJ12312a.k";
+    testInsensitivePalindrom(a);
+    a = "Papir12 S";
+    testInsensitivePalindrom(a);
+
+    // #11
+
+    std::list<std::string> lis {"Ala", "Kot", "Ma", "Rysia", "Ala",
+        "Sierotka", "Kot", "Ma", "Ala"};
+    std::deque<int> kolejka{
+        1, 2, 3, 4, 5, 3, 1, 2, 3, 4,
+        5, 2, 3, 1, 1, 2, 3, 2, 1, 4};
+
+    std::map<int, std::string> mapa = converter(lis,kolejka);
+    
+    std::for_each(mapa.begin(),mapa.end(),[](auto a){std::cout<<a.first<<" "<<a.second<<"   ";});
+
+
     return 0;
 }
