a286ead22c8e16c825f879f2825df71062d789c2
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-11-27 14:37:32 +0100

    FormatIngredients and FormatRecipit added

--- STL/Exercise1_AppendNewRecipe/AppendNewRecipe.cpp
+++ STL/Exercise1_AppendNewRecipe/AppendNewRecipe.cpp
@@ -1,18 +1,54 @@
 #include "AppendNewRecipe.hpp"
+#include <algorithm>
 #include <sstream> 
+#include <iterator>
 
-bool AppendNewRecipe(std::vector<std::string> steps,
-                          const std::list<std::string>& ingredients,
-                          const std::deque<std::pair<size_t, char>>& amount)
-                          {
-                            return true;
-                          }
+std::string convertCharIntoString(char zn){
+  
+  switch (zn)
+  {
+  case 'g':
+    return " gram";
+    break;
+  case 's':
+    return " szklanka(i)";
+    break;
+  case 'm':
+    return " mililitrow";
+    break;
+  
+  default:
+    return " Non-expected ingredient";
+    break;
+  }
+  return " Error";
+}
+
+std::vector<std::string> formatDequeAmount(const std::deque<std::pair<size_t, char>>& amount){
+  std::vector<std::string> formattedDeque;
+
+  std::for_each(amount.begin(),amount.end(),[&formattedDeque](const auto pair)
+  {
+    formattedDeque.push_back(std::to_string(pair.first)+convertCharIntoString(pair.second));
+  });
+
+  return formattedDeque;
+}
+
 
 std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
                                           const std::deque<std::pair<size_t, char>>& amount)
                                           {
-                                            std::vector<std::string> v{"ala","ma","kota"};
-                                            return v;
+                                            std::vector<std::string> FormatDeque = formatDequeAmount(amount);
+                                            std::vector<std::string> FormatIngredients;
+
+                                            std::transform(FormatDeque.begin(),FormatDeque.end(),ingredients.begin(),std::back_inserter(FormatIngredients),
+                                            [](const std::string& lhs, const std::string& rhs)
+                                            { 
+                                              return lhs + " " + rhs;
+                                            });
+                                            
+                                            return FormatIngredients;
                                           }
                             
 
@@ -20,7 +56,32 @@ std::stringstream FormatRecipit(std::vector<std::string> steps,
                                 const std::list<std::string>& ingredients,
                                 const std::deque<std::pair<size_t, char>>& amount)
                                 {
-                                    std::string ala{"ala"};
-                                    std::stringstream s(ala);
-                                    return s;
+                                    std::vector<std::string> formattedIngredients = FormatIngredients(ingredients, amount);
+                                    std::stringstream stream;
+                                    
+                                    stream<<"Skladniki:\n";
+                                    std::for_each(formattedIngredients.begin(),formattedIngredients.end(),
+                                    [&stream](const std::string& line)
+                                    {
+                                      stream<<line+",\n";
+                                    });
+
+                                    stream<<"\nKroki:\n";
+                                    std::for_each(steps.begin(),steps.end(),
+                                    [&stream,i{0}] (const std::string& line) mutable
+                                    {
+                                      stream<<++i<<") "+line+".\n";
+                                    });
+
+                                    
+                                    stream<<"___________________________________\n";
+                                    return stream;
                                 }
+
+
+bool AppendNewRecipe(std::vector<std::string> steps,
+                          const std::list<std::string>& ingredients,
+                          const std::deque<std::pair<size_t, char>>& amount)
+                          {
+                            return true;
+                          }
--- STL/Exercise1_AppendNewRecipe/AppendNewRecipe.hpp
+++ STL/Exercise1_AppendNewRecipe/AppendNewRecipe.hpp
@@ -3,6 +3,9 @@
 #include <deque>
 #include <vector>
 
+std::string convertCharIntoString(char zn);
+std::vector<std::string> formatDequeAmount(const std::deque<std::pair<size_t, char>>& amount);
+
 
 bool AppendNewRecipe(std::vector<std::string> steps,
                           const std::list<std::string>& ingredients,
