a889b4a5e0a78a3532d31b32a491d38a880164d1
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2023-01-15 19:01:05 +0100

    AllEven - fold Expression

--- Templates/Exercise7.cpp
+++ Templates/Exercise7.cpp
@@ -2,12 +2,21 @@
 
 template<typename... Args>
 void print(Args&&... args){
-    (std::cout<< ...<< args);
+    (std::cout<< ...<< args)<<"\n";
+}
+
+template<typename... Args>
+bool AllEven(Args&&... args){
+    print(args...);
+    return ((args % 2 == 0) && ...);
 }
 
 int main(){
 
     print(1.2, "Teks1",'c',5,500000);
+
+    std::cout<<std::boolalpha<<AllEven(2,4,6,8);
+    std::cout<<std::boolalpha<<AllEven(2,4,6,7);
 
 
     return 0;
